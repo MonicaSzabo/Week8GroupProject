223a8f0ddc02f1580831b5bb1f6179aa7d1e1314
Monica Szabo <monicaszb@gmail.com>
2016-06-11 11:37:28 -0500

    merge conflicts resolved

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,40 @@
 $(document).ready(function() {
+    var LatLong = [];
 
-	var queryCity = prompt("Enter a City");
+    function searchByCity() {
+        var queryCity = $('#city-input').val().trim();
+        var queryURL = "http://api.eventful.com/json/events/search?app_key=crQBBZznzX5Sn2R4&location=" + queryCity;
 
-	var queryURL = "http://api.eventful.com/json/events/search?app_key=crQBBZznzX5Sn2R4&location=" + queryCity;
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+            dataType: 'jsonp',
+            data: {
+                format: "jsonp",
+                apikey: 'crQBBZznzX5Sn2R4',
+            }
+        }).done(function(response){
+            var events = response.events.event;
+            var description = "";
 
-	$.ajax({
-        url: queryURL,
-        method: "GET",
-        dataType: 'jsonp',
-        data: {
-	        format: "jsonp",
-	        apikey: 'crQBBZznzX5Sn2R4',
-	    }
-    }).done(function(response){
-    	var events = response.events.event;
-    	var description = "";
+            for(var i = 0; i < events.length; i++) {
+                if(events[i].description === null) {
+                    description = "There is no description available";
+                }
+                else {
+                    description = events[i].description;
+                }
 
-    	for(var i = 0; i < events.length; i++) {
-    		if(events[i].description === null) {
-	    		description = "There is no description available";
-	    	}
-	    	else {
-	    		description = events[i].description;
-	    	}
+                $(".temp").append(events[i].title + "<br>Longitude: " + events[i].longitude +
+                    "<br>Latitude: " + events[i].latitude + "<br>Description: " + events[i].description + "<br>===========<br>");
+            }
 
-    		$(".temp").append(events[i].title + "<br>Longitude: " + events[i].longitude +
-    			"<br>Latitude: " + events[i].latitude + "<br>Description: " + events[i].description + "<br>===========<br>");
-    	}
+            
 
-    	console.log(events);
-    });
+            console.log(events);
+        });
 
+    }
+	
+    searchByCity();
 });
