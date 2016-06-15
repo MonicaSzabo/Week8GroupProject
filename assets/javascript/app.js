dcfe8a5e87ad54c3005037b1cf5a56e42164f6fe
Monica Szabo <monicaszb@gmail.com>
2016-06-14 21:52:21 -0500

    added jquery ui library and added autocomplete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,15 @@
 $(document).ready(function() {
     var mapData = [];
-
     var fb = new Firebase("https://events04.firebaseio.com/");
-    var cityStore = "";
 
+    $('#key').html("<img src='http://maps.google.com/mapfiles/ms/icons/green-dot.png'> = Today" +
+     "<img src='http://maps.google.com/mapfiles/ms/icons/blue-dot.png'> = This Week" +
+     "<img src='http://maps.google.com/mapfiles/ms/icons/red-dot.png'> = After Next Week");
 
     function searchByCity() {
     	$('#new-city').on('submit', function(){
 	        var queryCity = $('#city-input').val().trim();
 	        var queryURL = "http://api.eventful.com/json/events/search?app_key=crQBBZznzX5Sn2R4&location=" + queryCity;
-
-            fb.set({
-                city: queryCity
-            });
 
 	        mapData = [];
 	        $('#display').empty();
@@ -26,40 +23,81 @@ $(document).ready(function() {
 	                apikey: 'crQBBZznzX5Sn2R4',
 	            }
 	        }).done(function(response){
-	            var events = response.events.event;
-	            var description = "";
+                if(response.total_items > 0){
+                    var events = response.events.event;
+                    var description = "";
 
-	            for(var i = 0; i < events.length; i++) {
-                    //Calculates how far away the event is
-                    var daysDiff = moment(events[i].start_time).diff(moment(), "days");
+    	            for(var i = 0; i < events.length; i++) {
+                        //Calculates how many days until the event
+                        var daysDiff = moment(events[i].start_time).diff(moment(), "days");
 
-                    //Only will show days in the future, won't show past events
-                    if(daysDiff > -1) {
+                        //Only will show days in the future, won't show past events or Testing Event, a false event in the API
+                        if(daysDiff > -1 && events[i].title !== "Testing Event") {
 
-                        mapData.push({
-                            name: events[i].title,
-                            url: events[i].venue_url,
-                            lat: events[i].latitude,
-                            lng:  events[i].longitude,
-                            daysAway: daysDiff
-                        });
+                            mapData.push({
+                                name: events[i].title,
+                                url: events[i].venue_url,
+                                lat: events[i].latitude,
+                                lng:  events[i].longitude,
+                                daysAway: daysDiff
+                            });
 
 
-                        $('#display').append("<a href=" + events[i].venue_url +" class='eventLink' data-url=" +
-                            events[i].venue_url + " target='_blank'>" + events[i].title + "</a>" +
-                            "<br>" + events[i].venue_address + "<br>" + events[i].city_name + ", " + events[i].region_abbr +
-                            "<br>" + moment(events[i].start_time).format('MMMM Do YYYY, h:mm A') + "<br><br>");
-                    }
-	           	}
+                            $('#display').append("<a href=" + events[i].venue_url +" class='eventLink' data-url=" +
+                                events[i].venue_url + " target='_blank'>" + events[i].title + "</a>" +
+                                "<br>" + events[i].venue_address + "<br>" + events[i].city_name + ", " + events[i].region_abbr +
+                                "<br>" + moment(events[i].start_time).format('MMMM Do YYYY, h:mm A') + "<br><br>");
+                        }
+    	           	}
 
-	            console.log(mapData);
-                mapStuff(mapData);
+    	            console.log(mapData);
+                    mapStuff(mapData);
+                }
+                else {
+                    $('#display').append("Sorry! There are no events found for this request!");
+                }
 	        });
 
 	        return false;
 	    });
     }
 
+    function inDatabase(cityName) {
+
+        fb.push({
+            city: cityName
+        });
+    }
+
+    $(function() {
+        var availableTags = [
+            "ActionScript",
+            "AppleScript",
+            "Asp",
+            "BASIC",
+            "C",
+            "C++",
+            "Clojure",
+            "COBOL",
+            "ColdFusion",
+            "Erlang",
+            "Fortran",
+            "Groovy",
+            "Haskell",
+            "Java",
+            "JavaScript",
+            "Lisp",
+            "Perl",
+            "PHP",
+            "Python",
+            "Ruby",
+            "Scala",
+            "Scheme"
+        ];
+        $("#city-input").autocomplete({
+            source: availableTags
+        });
+    });
 
     searchByCity();
 
